6fbc63799700f835570010931974ba651b324449
Jeromnimo <jerome@bausse.fr>
2013-10-22 23:46:23 +0200

    WMV-229: Move ModelEvent into a dedicated file - add forgotten file

---
+++ src/ModelEvent.cpp
@@ -0,0 +1,13 @@
+/*
+ * ModelEvent.cpp
+ *
+ *  Created on: 22 oct. 2013
+ *
+ */
+
+#include "ModelEvent.h"
+
+void ModelEvent::init(MPQFile &f, ModelEventDef &me, uint32 *globals)
+{
+	def = me;
+}
