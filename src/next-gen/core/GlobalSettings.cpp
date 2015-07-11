67b8187e5559a199fb0aae4910074926eac90227
Jeromnimo <jerome@bausse.fr>
2015-07-11 09:14:40 +0200

    Update version to beta8

--- src/next-gen/core/GlobalSettings.cpp
+++ src/next-gen/core/GlobalSettings.cpp
@@ -111,7 +111,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".beta7";
+  m_versionSpecialExtend = ".beta8";
 
   bShowParticle = false;
   bZeroParticle = false;
