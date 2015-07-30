c195bf7075781b9877b274139c60f72d342711e9
Jeromnimo <jerome@bausse.fr>
2015-07-30 03:43:36 +0200

    Adapt project to use SDK again (instead of hard coded paths)

--- src/cmake/windows.cmake
+++ src/cmake/windows.cmake
@@ -1,8 +1,3 @@
-string (REPLACE "\\" "/" WMV_SDK_BASEDIR $ENV{WMV_SDK_BASEDIR})
-
-#put here full path to your makensis cmd
-set(MAKENSISCMD ${WMV_SDK_BASEDIR}/NSIS/makensis.exe)
-
 set(RES_FILES "wmv_mingw.rc")
 #set(CMAKE_RC_COMPILER_INIT windres)
 #ENABLE_LANGUAGE(RC)
@@ -14,8 +9,6 @@ message(STATUS "Using glew")
 include(glew/glew.cmake)
 add_definitions(-DGLEW_STATIC)
 set(WOWMV_SOURCES ${WOWMV_SOURCES} ${GLEW_SRC})
-message("glew src : ${GLEW_SRC}")
-message("glew include : ${GLEW_INCLUDE_DIR}")
 
 include_directories(${CMAKE_SOURCE_DIR}
                     ${wxWidgets_INCLUDE_DIRS}
@@ -31,10 +24,10 @@ add_definitions(-D_MINGW)
 add_definitions(-D_BETAVERSION) # comment if you are building a released version
 #add_definitions(-D_ALPHAVERSION) # comment if you are building a released version
 if(${CMAKE_BUILD_TYPE} MATCHES MinSizeRel)
-  message("Release build : Final exe will be stripped")
+  message(STATUS "Release build : Final exe will be stripped")
   set(CMAKE_EXE_LINKER_FLAGS "-s") # strip exe
 else()
-  message("${CMAKE_BUILD_TYPE} build : Final exe will NOT be stripped")
+  message(STATUS "${CMAKE_BUILD_TYPE} build : Final exe will NOT be stripped")
 endif()
   
 # disable some visual studio annoying warnings
@@ -60,15 +53,14 @@ target_link_libraries(wowmodelviewer
 )
 
 add_custom_target(release nmake install
-                  COMMAND ${MAKENSISCMD} "../Installers/Windows/NSIS/WMVInstallerMUI.nsi")             
+                  COMMAND $ENV{WMV_SDK_BASEDIR}/NSIS/makensis.exe "../Installers/Windows/NSIS/WMVInstallerMUI.nsi")             
 
 
 install(TARGETS wowmodelviewer 
         RUNTIME DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/../bin)
   
 # additional files needed to let WMV correctly works
-#set(QT_BIN_DIR D:/Programmes/QT_VS/5.3/msvc2013_opengl/bin)
-set(QT_BIN_DIR  D:/Programmes/QT_VS_5.4.2/5.4/msvc2013_opengl/bin)
+set(QT_BIN_DIR  $ENV{WMV_SDK_BASEDIR}/Qt/bin)
 set(QT_FILES ${QT_BIN_DIR}/Qt5Core.dll 
   		     ${QT_BIN_DIR}/Qt5Gui.dll
   		     ${QT_BIN_DIR}/Qt5Network.dll
@@ -78,10 +70,10 @@ set(QT_FILES ${QT_BIN_DIR}/Qt5Core.dll
 set(QT_SYS_FILES ${QT_BIN_DIR}/icudt53.dll
   			     ${QT_BIN_DIR}/icuin53.dll
   			     ${QT_BIN_DIR}/icuuc53.dll
-  			     D:/sdk_new/bin_support/msvcp120.dll
-  			     D:/sdk_new/bin_support/msvcr120.dll)
+  			     $ENV{WMV_SDK_BASEDIR}/bin_support/msvcp120.dll
+  			     $ENV{WMV_SDK_BASEDIR}/bin_support/msvcr120.dll)
   				 
-set(QT_PLUGIN_DIR D:/Programmes/QT_VS_5.4.2/5.4/msvc2013_opengl/plugins)
+set(QT_PLUGIN_DIR $ENV{WMV_SDK_BASEDIR}/Qt/plugins)
 set(QT_PLUGIN_SYS_FILES ${QT_PLUGIN_DIR}/platforms/qminimal.dll
                         ${QT_PLUGIN_DIR}/platforms/qoffscreen.dll
                         ${QT_PLUGIN_DIR}/platforms/qwindows.dll)
