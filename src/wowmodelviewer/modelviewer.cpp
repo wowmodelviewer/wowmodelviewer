16983cb216cf5f191c949672a5c55381f8fe2dfc
Jeromnimo <jerome@bausse.fr>
2015-08-23 18:05:18 +0200

    Select all image types by default when loadng background (Patch from Wain)

--- src/wowmodelviewer/modelviewer.cpp
+++ src/wowmodelviewer/modelviewer.cpp
@@ -1933,7 +1933,7 @@ void ModelViewer::OnBackground(wxCommandEvent &event)
 
 	if (id == ID_BACKGROUND) {
 		if (event.IsChecked()) {
-			wxFileDialog dialog(this, wxT("Load Background"), dir.GetPath(wxPATH_GET_VOLUME), wxEmptyString, wxT("Bitmap Images (*.bmp)|*.bmp|Jpeg Images (*.jpg)|*.jpg|PNG Images (*.png)|*.png|AVI Video file(*.avi)|*.avi"));
+		  wxFileDialog dialog(this, wxT("Load Background"), dir.GetPath(wxPATH_GET_VOLUME), wxEmptyString, wxT("All (*.bmp;*.jpg;*.png;*.avi)|*.bmp;*.jpg;*.png;*.avi|Bitmap Images (*.bmp)|*.bmp|Jpeg Images (*.jpg)|*.jpg|PNG Images (*.png)|*.png|AVI Video file(*.avi)|*.avi"));
 			if (dialog.ShowModal() == wxID_OK) {
 				canvas->LoadBackground(dialog.GetPath());
 				dir.SetPath(dialog.GetPath());
