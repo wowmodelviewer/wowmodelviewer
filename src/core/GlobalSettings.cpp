fa1730faebe63abad720f077293642e9f8d12e4e
Jeromnimo <jerome@bausse.fr>
2015-08-26 23:05:12 +0200

    Update version to rc1, remove beta mode

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -111,7 +111,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".beta10";
+  m_versionSpecialExtend = ".rc1";
 
   bShowParticle = false;
   bZeroParticle = false;
