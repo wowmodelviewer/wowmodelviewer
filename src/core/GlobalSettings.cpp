a623b5c7073a60638a4f0acf5dda53072d56fc08
Jeromnimo <jerome@bausse.fr>
2015-12-06 14:53:57 +0100

    update version to 0.8.0

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -111,7 +111,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".rc5";
+  m_versionSpecialExtend = "";
 
   bShowParticle = false;
   bZeroParticle = false;
