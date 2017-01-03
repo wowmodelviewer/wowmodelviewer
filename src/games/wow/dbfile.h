c60731d7fc09b1829c591eeea6c07ee19cb9d330
Jeromnimo <jerome@bausse.fr>
2017-01-03 22:21:16 +0100

    Fix debug compilation

--- src/games/wow/dbfile.h
+++ src/games/wow/dbfile.h
@@ -81,33 +81,27 @@ public:
 
   float getFloat(unsigned char * recordOffset, size_t field) const
   {
-    assert(field < file.fieldCount);
     return *reinterpret_cast<float*>(recordOffset + field * 4);
   }
   unsigned int getUInt(unsigned char * recordOffset, size_t field) const
   {
-    assert(field < file.fieldCount);
     return *reinterpret_cast<unsigned int*>(recordOffset + (field * 4));
   }
   int getInt(unsigned char * recordOffset, size_t field) const
   {
-    assert(field < file.fieldCount);
     return *reinterpret_cast<int*>(recordOffset + field * 4);
   }
   unsigned char getByte(unsigned char * recordOffset, size_t ofs) const
   {
-    assert(ofs < file.recordSize);
     return *reinterpret_cast<unsigned char*>(recordOffset + ofs);
   }
 
 
   std::string getStdString(unsigned char * recordOffset, size_t field) const
   {
-    assert(field < file.fieldCount);
     size_t stringOffset = getUInt(recordOffset, field);
     if (stringOffset >= stringSize)
       stringOffset = 0;
-    assert(stringOffset < file.stringSize);
 
     return std::string(reinterpret_cast<char*>(stringTable + stringOffset));
   }
