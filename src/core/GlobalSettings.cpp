d421ae4f081a7878bd253ffc7f261dd766331914
Jeromnimo <jerome@bausse.fr>
2017-02-14 22:24:49 +0100

    Update version to alpha6

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".alpha5";
+  m_versionSpecialExtend = ".alpha6";
 
   bShowParticle = false;
   bZeroParticle = false;
