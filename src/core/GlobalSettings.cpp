d221cbbaca848a67a7db1b4eb531156c0953e54f
Jeromnimo <jerome@bausse.fr>
2017-03-16 22:49:24 +0100

    Update version to alpha8

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".alpha7";
+  m_versionSpecialExtend = ".alpha8";
 
   bShowParticle = false;
   bZeroParticle = false;
