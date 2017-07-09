ccd5e85804474cc3b1db5b550016458657b345d5
Jeromnimo <jerome@bausse.fr>
2017-07-09 14:40:48 +0200

    Update version to alpha13

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".alpha12";
+  m_versionSpecialExtend = ".alpha13";
 
   bShowParticle = false;
   bZeroParticle = false;
