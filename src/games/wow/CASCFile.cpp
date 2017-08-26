ea86f19f0d374c5cc5a58515640d3663a647104d
Jeromnimo <jerome@bausse.fr>
2017-08-26 15:57:41 +0200

    Multi chunk support - first patch

--- src/games/wow/CASCFile.cpp
+++ src/games/wow/CASCFile.cpp
@@ -23,6 +23,12 @@ CASCFile::~CASCFile()
   close();
 }
 
+struct chunkHeader
+{
+  char magic[4];
+  unsigned __int32 size;
+};
+
 
 bool CASCFile::open()
 {
@@ -61,39 +67,54 @@ bool CASCFile::open()
       delete [] buffer;
 
     buffer = new unsigned char[size];
-    if(!CascReadFile(m_handle, buffer, size, &nbBytesRead))
+    if (!CascReadFile(m_handle, buffer, size, &nbBytesRead))
     {
       LOG_ERROR << "Reading" << filepath << "failed." << "Error" << GetLastError();
+      return false;
     }
 
-    if(nbBytesRead != 0)
+    if (nbBytesRead != 0)
       eof = false;
-    else
-      eof = true;
+
+    // md21 file, need to read all chunks
+    if ((nbBytesRead >= 4) && (buffer[0] == 'M' && buffer[1] == 'D' && buffer[2] == '2' && buffer[3] == '1'))
+    {
+      unsigned char * bufferPtr = buffer;
+
+      unsigned char * md21chunkStart = 0;
+      unsigned __int32 md21chunkSize = 0;
+
+      LOG_INFO << "Parsing chunks for file" << filepath;
+      while (bufferPtr < buffer + nbBytesRead)
+      {
+        chunkHeader chunkHead;
+        memcpy(&chunkHead, bufferPtr, sizeof(chunkHeader));
+        bufferPtr += sizeof(chunkHeader);
+        LOG_INFO << "Chunk :" << chunkHead.magic[0] << chunkHead.magic[1] << chunkHead.magic[2] << chunkHead.magic[3] << chunkHead.size;
+
+        if (chunkHead.magic[0] == 'M' && chunkHead.magic[1] == 'D' && chunkHead.magic[2] == '2' && chunkHead.magic[3] == '1')
+        {
+          md21chunkStart = bufferPtr;
+          md21chunkSize = chunkHead.size;
+        }
+
+        bufferPtr += chunkHead.size;
+      }
+
+      // relocate buffer on MD21 chunk
+      if (md21chunkSize != 0)
+      {
+        // relocate buffer on chunk's data
+        unsigned char *newBuffer = new unsigned char[md21chunkSize];
+        memcpy(newBuffer, md21chunkStart, md21chunkSize);
+        delete[] buffer;
+        buffer = newBuffer;
+        size = md21chunkSize; // real size is only chunk' size
+      }
+    }
 #ifdef DEBUG_READ
     LOG_INFO << __FUNCTION__ <<  "|" << filepath << "nb bytes read =>" << nbBytesRead << "/" << size;
 #endif
-
-    // if MD21 file, deal with chunks
-    if ((nbBytesRead >= 4) && (buffer[0] == 'M' && buffer[1] == 'D' && buffer[2] == '2' && buffer[3] == '1'))
-    {
-      // read size of first chunk in the file
-      unsigned __int32 chunkSize;
-      memcpy(&chunkSize, buffer + 4, 4);
-
-      // check if there is only one chunk in the file (if chunk size is at least 98% of the file)
-      if ((float)chunkSize / (float)size >= 0.98)
-      {
-        LOG_INFO << __FUNCTION__ << __FILE__ << __LINE__ << "MD21 file detected";
-        // relocate buffer on chunk's data
-        unsigned char *newBuffer = new unsigned char[chunkSize];
-        std::copy(buffer + 8, buffer + 8 + chunkSize, newBuffer);
-        delete[] buffer;
-        buffer = newBuffer;
-        size = chunkSize; // real size is only chunk' size
-      }
-      // @todo else -> multiple chunks in the file, to be implemented when needed
-    }
   }
 
   return true;
