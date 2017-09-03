55d6783797a227e99c46b89c5360e5a974441be7
Jeromnimo <jerome@bausse.fr>
2017-09-03 17:16:49 +0200

    Update version to alpha17

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ core::GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".alpha16";
+  m_versionSpecialExtend = ".alpha17";
 
   bShowParticle = false;
   bZeroParticle = false;
