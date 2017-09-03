6b17dfe4f34730107ee628af718dfc480ac623e4
Jeromnimo <jerome@bausse.fr>
2017-09-03 16:43:28 +0200

    Automatically read chunk for all files (if applicable) ==> fix EmoteX animation for characters

--- src/games/wow/CASCFile.cpp
+++ src/games/wow/CASCFile.cpp
@@ -12,6 +12,68 @@
 #include "logger/Logger.h"
 // #define DEBUG_READ
 
+std::vector<std::string> KNOWN_CHUNKS =
+{
+  "PFID",
+  "SFID",
+  "AFID",
+  "BFID",
+  "MD21",
+  "TXAC",
+  "EXPT",
+  "EXP2",
+  "PABC",
+  "PADC",
+  "PSBC",
+  "PEDC",
+  "SKID",
+  "AFM2",
+  "AFSA",
+  "AFSB",
+  /*
+  "MOHD",
+  "MOTX",
+  "MOMT",
+  "MOUV",
+  "MOGN",
+  "MOGI",
+  "MOSB",
+  "MOPV",
+  "MOPT",
+  "MOPR",
+  "MOVV",
+  "MOVB",
+  "MOLT",
+  "MODS",
+  "MODN",
+  "MODD",
+  "MFOG",
+  "MCVP",
+  "GFID",
+  "MOGP",
+  "MOPY",
+  "MOVI",
+  "MOVT",
+  "MONR",
+  "MOTV",
+  "MOBA",
+  "MOLR",
+  "MODR",
+  "MOBN",
+  "MOBR",
+  "MOCV",
+  "MLIQ",
+  "MORI",
+  "MORB",
+  "MOTA",
+  "MOBS",
+  "MDAL",
+  "MOPL",
+  "MOPB",
+  "MOLS",
+  "MOLP"
+  */
+};
 
 CASCFile::CASCFile(QString path, int id)
   : GameFile(path, id), m_handle(0)
@@ -73,28 +135,38 @@ unsigned long CASCFile::readFile()
 
 void CASCFile::doPostOpenOperation()
 {
-  // md21 file, need to read all chunks
-  if ((size >= 4) && (buffer[0] == 'M' && buffer[1] == 'D' && buffer[2] == '2' && buffer[3] == '1'))
+  if (size >= sizeof(chunkHeader))
   {
-    unsigned int offset = 0;
-
-    LOG_INFO << "Parsing chunks for file" << filepath;
-    while (offset < size)
+    chunkHeader chunkHead;
+    memcpy(&chunkHead, buffer, sizeof(chunkHeader));
+    std::string magic = std::string(chunkHead.magic, 4);
+    if (std::find(KNOWN_CHUNKS.begin(), KNOWN_CHUNKS.end(), magic) != KNOWN_CHUNKS.end() 
+        && chunkHead.size <= size)
     {
-      chunkHeader chunkHead;
-      memcpy(&chunkHead, buffer + offset, sizeof(chunkHeader));
-      offset += sizeof(chunkHeader);
+      unsigned int offset = 0;
 
-      Chunk * chunk = new Chunk();
-      chunk->magic = std::string(chunkHead.magic, 4);
-      chunk->start = offset;
-      chunk->size = chunkHead.size;
-      chunk->pointer = 0;
-      chunks.push_back(*chunk);
+      LOG_INFO << "Parsing chunks for file" << filepath << "First chunk read :" << magic.c_str();
+      while (offset < size)
+      {
+        chunkHeader chunkHead;
+        memcpy(&chunkHead, buffer + offset, sizeof(chunkHeader));
+        offset += sizeof(chunkHeader);
 
-      LOG_INFO << "Chunk :" << chunk->magic.c_str() << chunk->size;
+        Chunk * chunk = new Chunk();
+        chunk->magic = std::string(chunkHead.magic, 4);
+        chunk->start = offset;
+        chunk->size = chunkHead.size;
+        chunk->pointer = 0;
+        chunks.push_back(*chunk);
 
-      offset += chunkHead.size;
+        LOG_INFO << "Chunk :" << chunk->magic.c_str() << chunk->start << chunk->size;
+
+        offset += chunkHead.size;
+      }
+
+      // if there is only one chunk, set it
+      if (chunks.size() == 1)
+        setChunk(chunks[0].magic);
     }
   }
 }
