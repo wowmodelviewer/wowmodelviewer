a98e1776ea0aafc6336956a42b40b00390c71886
Jeromnimo <jerome@bausse.fr>
2017-10-28 14:35:00 +0200

    Update version to beta3

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ core::GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".beta2";
+  m_versionSpecialExtend = ".beta3";
 
   bShowParticle = false;
   bZeroParticle = false;
