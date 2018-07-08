dc39852cd82bb0762e7fe929f9965f56f6cb0271
Jeromnimo <jerome@bausse.fr>
2018-07-08 14:14:31 +0200

    Update version to beta9

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ core::GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = ".beta8";
+  m_versionSpecialExtend = ".beta9";
 
   bShowParticle = false;
   bZeroParticle = false;
