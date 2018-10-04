81aa5c62bd93efff035d5c2ebe05bc78b796f1e0
Jeromnimo <jerome@bausse.fr>
2018-10-05 00:10:22 +0200

    Update version to beta10

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ core::GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = L".beta9";
+  m_versionSpecialExtend = L".beta10";
 
   bShowParticle = false;
   bZeroParticle = false;
