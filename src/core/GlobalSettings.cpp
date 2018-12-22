45dbba686d209f37ae9447fc9df88870e259d63b
Jeromnimo <jerome@bausse.fr>
2018-12-22 16:30:06 +0100

    Update version to beta13

--- src/core/GlobalSettings.cpp
+++ src/core/GlobalSettings.cpp
@@ -121,7 +121,7 @@ core::GlobalSettings::GlobalSettings()
 
 
   // set it to whatever you want if you are building a special version
-  m_versionSpecialExtend = L".beta12";
+  m_versionSpecialExtend = L".beta13";
 
   bShowParticle = false;
   bZeroParticle = false;
