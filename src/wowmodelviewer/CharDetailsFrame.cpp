5625a5f33b0e14eec8a90e7f3131688564d2004a
WainPetopia <25504585+WainPetopia@users.noreply.github.com>
2019-11-04 19:18:52 +1100

    WMV-494 - "Randomise" option for character models should work on Custom1-3 sections too

--- src/wowmodelviewer/CharDetailsFrame.cpp
+++ src/wowmodelviewer/CharDetailsFrame.cpp
@@ -125,7 +125,10 @@ void CharDetailsFrame::randomiseChar()
   m_model->cd.setRandomValue(CharDetails::FACIAL_CUSTOMIZATION_COLOR);
   m_model->cd.setRandomValue(CharDetails::ADDITIONAL_FACIAL_CUSTOMIZATION);
 
-  if (m_model->cd.isDemonHunter())
+  // Don't worry about Custom 1-3 for elves, unless they're Demon Hunters:
+  RaceInfos infos;
+  if (m_model->cd.isDemonHunter() || (RaceInfos::getCurrent(m_model, infos) &&
+      infos.raceid != RACE_NIGHTELF && infos.raceid != RACE_BLOODELF))
   {
     m_model->cd.setRandomValue(CharDetails::CUSTOM1_STYLE);
     m_model->cd.setRandomValue(CharDetails::CUSTOM1_COLOR);
