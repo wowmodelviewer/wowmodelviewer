d1dc1df4e60bc8cffa4be7dc1a3cf5f3598ef2a1
Jeromnimo <jerome@bausse.fr>
2020-09-26 18:19:35 +0200

    WMV-550: update database definition and update RaceInfos builidng

--- src/games/wow/RaceInfos.cpp
+++ src/games/wow/RaceInfos.cpp
@@ -6,7 +6,7 @@
 
 #include "logger/Logger.h"
 
-#define DEBUG_RACEINFOS 0
+#define DEBUG_RACEINFOS 1
 
 std::map<int, RaceInfos> RaceInfos::RACES;
 
@@ -31,7 +31,7 @@ bool RaceInfos::getCurrent(WoWModel * model, RaceInfos & result)
     LOG_ERROR << __FUNCTION__ << "model is null";
     return false;
   }
-  int fdid = model->gamefile->fileDataId();
+  auto fdid = model->gamefile->fileDataId();
   if (SDReplacementModel.count(fdid)) // if it's an old *_sdr model, use the file ID of its HD counterpart for race info
     fdid = SDReplacementModel[fdid];
   const auto raceInfosIt = RACES.find(fdid);
@@ -48,91 +48,149 @@ bool RaceInfos::getCurrent(WoWModel * model, RaceInfos & result)
 
 void RaceInfos::init()
 {
-  sqlResult races = 
-    GAMEDATABASE.sqlQuery("SELECT CMDM.FileID as malemodel, lower(ClientPrefix), CharComponentTexLayoutID, "
-                          "CMDF.FileID AS femalemodel, lower(ClientPrefix), CharComponentTexLayoutID, "
-                          "CMDMHD.FileID as malemodelHD, lower(ClientPrefix), CharComponentTexLayoutHiResID, "
-                          "CMDFHD.FileID AS femalemodelHD, lower(ClientPrefix), CharComponentTexLayoutHiResID, "
-                          "ChrRaces.ID, BaseRaceID, Flags, MaleModelFallbackRaceID, MaleModelFallbackSex, "
-                          "FemaleModelFallbackRaceID, FemaleModelFallbackSex, MaleTextureFallbackRaceID, MaleTextureFallbackSex, "
-                          "FemaleTextureFallbackRaceID, FemaleTextureFallbackSex FROM ChrRaces "
-                          "LEFT JOIN CreatureDisplayInfo CDIM ON CDIM.ID = MaleDisplayID LEFT JOIN CreatureModelData CMDM ON CDIM.ModelID = CMDM.ID "
-                          "LEFT JOIN CreatureDisplayInfo CDIF ON CDIF.ID = FemaleDisplayID LEFT JOIN CreatureModelData CMDF ON CDIF.ModelID = CMDF.ID "
-                          "LEFT JOIN CreatureDisplayInfo CDIMHD ON CDIMHD.ID = HighResMaleDisplayId LEFT JOIN CreatureModelData CMDMHD ON CDIMHD.ModelID = CMDMHD.ID "
-                          "LEFT JOIN CreatureDisplayInfo CDIFHD ON CDIFHD.ID = HighResFemaleDisplayId LEFT JOIN CreatureModelData CMDFHD ON CDIFHD.ModelID = CMDFHD.ID");
+  // check database version (8.x is different from 9.x on races management)
+  auto checkVersion = GAMEDATABASE.sqlQuery("SELECT * FROM sqlite_master WHERE type='table' AND name='ChrModel'");
 
-  if(!races.valid || races.empty())
+  // if 'ChrModel' table does not exist, it's a 8.x game, otherwise, it's a 9.x
+  if (checkVersion.empty())
   {
-    LOG_ERROR << "Unable to collect race information from game database";
-    return;
-  }
+    auto races =
+      GAMEDATABASE.sqlQuery("SELECT CMDM.FileID as malemodel, lower(ClientPrefix), CharComponentTexLayoutID, "
+        "CMDF.FileID AS femalemodel, lower(ClientPrefix), CharComponentTexLayoutID, "
+        "CMDMHD.FileID as malemodelHD, lower(ClientPrefix), CharComponentTexLayoutHiResID, "
+        "CMDFHD.FileID AS femalemodelHD, lower(ClientPrefix), CharComponentTexLayoutHiResID, "
+        "ChrRaces.ID, BaseRaceID, Flags, MaleModelFallbackRaceID, MaleModelFallbackSex, "
+        "FemaleModelFallbackRaceID, FemaleModelFallbackSex, MaleTextureFallbackRaceID, MaleTextureFallbackSex, "
+        "FemaleTextureFallbackRaceID, FemaleTextureFallbackSex FROM ChrRaces "
+        "LEFT JOIN CreatureDisplayInfo CDIM ON CDIM.ID = MaleDisplayID LEFT JOIN CreatureModelData CMDM ON CDIM.ModelID = CMDM.ID "
+        "LEFT JOIN CreatureDisplayInfo CDIF ON CDIF.ID = FemaleDisplayID LEFT JOIN CreatureModelData CMDF ON CDIF.ModelID = CMDF.ID "
+        "LEFT JOIN CreatureDisplayInfo CDIMHD ON CDIMHD.ID = HighResMaleDisplayId LEFT JOIN CreatureModelData CMDMHD ON CDIMHD.ModelID = CMDMHD.ID "
+        "LEFT JOIN CreatureDisplayInfo CDIFHD ON CDIFHD.ID = HighResFemaleDisplayId LEFT JOIN CreatureModelData CMDFHD ON CDIFHD.ModelID = CMDFHD.ID");
 
-  for (auto& value : races.values)
-  {
-    std::string displayPrefix;
-
-    for(int r = 0; r <12 ; r+=3)
+    if (!races.valid || races.empty())
     {
-      if(value[r] != "")
+      LOG_ERROR << "Unable to collect race information from game database";
+      return;
+    }
+
+    for (auto& value : races.values)
+    {
+      for (auto r = 0; r < 12; r += 3)
       {
-        RaceInfos infos;
-        infos.prefix = !displayPrefix.empty() ? displayPrefix : value[r + 1].toStdString();
-        infos.textureLayoutID = value[r+2].toInt();
-        infos.raceID = value[12].toInt();
-        infos.sexID = (r == 0 || r == 6)?GENDER_MALE:GENDER_FEMALE;
-        infos.barefeet = (value[14].toInt() & 0x2);
-        // Get fallback display race ID (this is mostly for allied races and others that rely on
-        // item display info from other race models):
-      	if(infos.sexID == GENDER_MALE)
-      	{
-      		infos.modelFallbackRaceID = value[15].toInt();
-          infos.modelFallbackSexID = value[16].toInt();
-          infos.textureFallbackRaceID = value[19].toInt();
-          infos.textureFallbackSexID = value[20].toInt();
-      	}
-        else
+        if (value[r] != "")
         {
-          infos.modelFallbackRaceID = value[17].toInt();
-          infos.modelFallbackSexID = value[18].toInt();
-          infos.textureFallbackRaceID = value[21].toInt();
-          infos.textureFallbackSexID = value[22].toInt();
+          RaceInfos infos;
+          infos.prefix = value[r + 1].toStdString();
+          infos.textureLayoutID = value[r + 2].toInt();
+          infos.raceID = value[12].toInt();
+          infos.sexID = (r == 0 || r == 6) ? GENDER_MALE : GENDER_FEMALE;
+          infos.barefeet = (value[14].toInt() & 0x2);
+          // Get fallback display race ID (this is mostly for allied races and others that rely on
+          // item display info from other race models):
+          if (infos.sexID == GENDER_MALE)
+          {
+            infos.modelFallbackRaceID = value[15].toInt();
+            infos.modelFallbackSexID = value[16].toInt();
+            infos.textureFallbackRaceID = value[19].toInt();
+            infos.textureFallbackSexID = value[20].toInt();
+          }
+          else
+          {
+            infos.modelFallbackRaceID = value[17].toInt();
+            infos.modelFallbackSexID = value[18].toInt();
+            infos.textureFallbackRaceID = value[21].toInt();
+            infos.textureFallbackSexID = value[22].toInt();
+          }
+
+          auto modelfileid = value[r].toInt();
+
+          if ((r == 6) || (r == 9)) // if we are dealing with a HD model
+            infos.isHD = true;
+          else
+            infos.isHD = false;
+
+          if (RACES.find(modelfileid) == RACES.end())
+            RACES[modelfileid] = infos;
+
+          // cheap workaround to include the upright male orc model, because I
+          // can't find anything in the database that links it to the orc races:
+          if (infos.raceID == RACE_ORC && infos.sexID == GENDER_MALE && infos.isHD)
+          {
+            RACES[1968587] = infos;
+          }
         }
-
-        int modelfileid = value[r].toInt();
-        
-        if ((r == 6) || (r == 9)) // if we are dealing with a HD model
-          infos.isHD = true;
-        else
-          infos.isHD = false;
-
-        if (RACES.find(modelfileid) == RACES.end())
-          RACES[modelfileid] = infos;
-        
-         // cheap workaround to include the upright male orc model, because I
-         // can't find anything in the database that links it to the orc races:
-        if (infos.raceID == RACE_ORC && infos.sexID == GENDER_MALE && infos.isHD)
-        {
-          RACES[1968587] = infos;
-        }
-
-#if DEBUG_RACEINFOS > 0
-        LOG_INFO << "---------------------------";
-        LOG_INFO << "modelfileid ->" << modelfileid;
-        LOG_INFO << "infos.prefix =" << infos.prefix.c_str();
-        LOG_INFO << "infos.textureLayoutID =" << infos.textureLayoutID;
-        LOG_INFO << "infos.raceID =" << infos.raceID;
-        LOG_INFO << "infos.sexID =" << infos.sexID;
-        LOG_INFO << "infos.isHD =" << infos.isHD;
-        LOG_INFO << "infos.modelFallbackRaceID =" << infos.modelFallbackRaceID;
-        LOG_INFO << "infos.modelFallbackSexID =" << infos.modelFallbackSexID;
-        LOG_INFO << "infos.textureFallbackRaceID =" << infos.textureFallbackRaceID;
-        LOG_INFO << "infos.textureFallbackSexID =" << infos.textureFallbackSexID;
-        LOG_INFO << "---------------------------";
-#endif
       }
     }
   }
+  else // we are dealing with a 9.x database
+  {
+    auto races =
+      GAMEDATABASE.sqlQuery("SELECT ChrRaces.ClientPrefix, ChrRaces.ID, ChrRaces.Flags, ChrModel.Sex, CreatureModelData.FileID, ChrModel.CharComponentTextureLayoutID, "
+                            "ChrRaces.MaleModelFallbackRaceID, ChrRaces.MaleModelFallbackSex, ChrRaces.MaleTextureFallbackRaceID, ChrRaces.MaleTextureFallbackSex, "
+                            "ChrRaces.FemaleModelFallbackRaceID, ChrRaces.FemaleModelFallbackSex, ChrRaces.FemaleTextureFallbackRaceID, ChrRaces.FemaleTextureFallbackSex "
+                            "FROM ChrRaceXChrModel "
+                            "LEFT JOIN ChrRaces ON ChrRaces.ID = ChrRaceXChrModel.ChrRacesID "
+                            "LEFT JOIN ChrModel ON ChrModel.ID = ChrRaceXChrModel.ChrModelID "
+                            "LEFT JOIN CreatureDisplayInfo ON CreatureDisplayInfo.ID = ChrModel.DisplayID "
+                            "LEFT JOIN CreatureModelData ON CreatureModelData.ID = CreatureDisplayInfo.ModelID ");
 
+    if (!races.valid || races.empty())
+    {
+      LOG_ERROR << "Unable to collect race information from game database";
+      return;
+    }
+
+    for (auto& race : races.values)
+    {
+      RaceInfos infos;
+      infos.prefix = race[0].toStdString();
+      infos.raceID = race[1].toInt();
+      infos.barefeet = (race[2].toInt() & 0x2);
+      infos.sexID = race[3].toInt();
+      auto modelfileid = race[4].toInt();
+      infos.textureLayoutID = race[5].toInt();
+
+      // Get fallback display race ID (this is mostly for allied races and others that rely on
+     // item display info from other race models):
+      if (infos.sexID == GENDER_MALE)
+      {
+        infos.modelFallbackRaceID = race[6].toInt();
+        infos.modelFallbackSexID = race[7].toInt();
+        infos.textureFallbackRaceID = race[8].toInt();
+        infos.textureFallbackSexID = race[9].toInt();
+      }
+      else
+      {
+        infos.modelFallbackRaceID = race[10].toInt();
+        infos.modelFallbackSexID = race[11].toInt();
+        infos.textureFallbackRaceID = race[12].toInt();
+        infos.textureFallbackSexID = race[13].toInt();
+      }
+
+      infos.isHD = true; // todo
+
+      if (RACES.find(modelfileid) == RACES.end())
+        RACES[modelfileid] = infos;
+    }
+  }
+
+#if DEBUG_RACEINFOS > 0
+  for (const auto & r : RACES)
+  {
+    LOG_INFO << "---------------------------";
+    LOG_INFO << "modelfileid ->" << r.first;
+    LOG_INFO << "infos.prefix =" << r.second.prefix.c_str();
+    LOG_INFO << "infos.textureLayoutID =" << r.second.textureLayoutID;
+    LOG_INFO << "infos.raceID =" << r.second.raceID;
+    LOG_INFO << "infos.sexID =" << r.second.sexID;
+    LOG_INFO << "infos.isHD =" << r.second.isHD;
+    LOG_INFO << "infos.modelFallbackRaceID =" << r.second.modelFallbackRaceID;
+    LOG_INFO << "infos.modelFallbackSexID =" << r.second.modelFallbackSexID;
+    LOG_INFO << "infos.textureFallbackRaceID =" << r.second.textureFallbackRaceID;
+    LOG_INFO << "infos.textureFallbackSexID =" << r.second.textureFallbackSexID;
+    LOG_INFO << "---------------------------";
+  }
+#endif
 }
 
 int RaceInfos::getHDModelForFileID(int fileid)
