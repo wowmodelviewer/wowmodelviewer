f199d401efec51b9758678d2947c5032eb252bc1
natnat <nathan_mathew_carr@hotmail.co.uk>
2022-08-07 04:13:18 +0100

    Removed all duplicate calls to find_package for QT

--- src/cmake/common.cmake
+++ src/cmake/common.cmake
@@ -11,13 +11,6 @@ SET(WMV_CMAKE_FOLDER ${WMV_BASE_PATH}/src/cmake)
 # define policies to avoid warnings
 include(${WMV_CMAKE_FOLDER}/policies.cmake)
 
-# Qt5 stuff
-# init cmake with our qt install directory
-SET(QT_LOCATION C:/Qt/5.13.2/msvc2017)
-SET(QT_VERSION 5.13.2)
-SET(CMAKE_PREFIX_PATH ${QT_LOCATION}/lib/cmake)
-
-
 #############################
 #  platform specific part   #
 #############################
@@ -44,19 +37,12 @@ macro(use_wow)
   use_casclib() # if you use wow lib, you are underneath using casc lib 
   include_directories(${WMV_BASE_PATH}/src/games/wow)
   link_directories(${QT_LOCATION}/lib)
-  find_package(Qt5Core ${QT_VERSION})
-  find_package(Qt5Xml ${QT_VERSION})
-  find_package(Qt5Gui ${QT_VERSION})
-  find_package(Qt5Network ${QT_VERSION})
   list(APPEND extralibs wow)
 endmacro()
 
 macro(use_core)
   include_directories(${WMV_BASE_PATH}/src/core)
   link_directories(${QT_LOCATION}/lib)
-  find_package(Qt5Core ${QT_VERSION}) 
-  find_package(Qt5Gui ${QT_VERSION}) # Qt5Gui is needed for QImage
-  find_package(Qt5Network ${QT_VERSION})
   list(APPEND extralibs core)
 endmacro()
   
