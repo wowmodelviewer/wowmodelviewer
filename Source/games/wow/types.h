271a2a82e6fe8ff7fecb28dde2f486b4211ef51a
natnat <nathan_mathew_carr@hotmail.co.uk>
2023-05-29 19:36:06 +0100

    Re-organised DB2 types to be more specific to dbdefs

--- Source/games/wow/types.h
+++ Source/games/wow/types.h
@@ -19,15 +19,15 @@
 
 
 #if defined(_WINDOWS)
-typedef unsigned char uint8;
-  typedef char int8;
-  typedef unsigned __int16 uint16;
-  typedef __int16 int16;
-  typedef unsigned __int32 uint32;
-  typedef __int32 int32;
-  typedef unsigned __int64 uint64;
-  typedef __int64 int64;
-  typedef int ssize_t;
+	typedef __int8 int8;
+	typedef __int16 int16;
+	typedef __int32 int32;
+	typedef __int64 int64;
+	typedef unsigned __int8 uint8;
+	typedef unsigned __int16 uint16;
+	typedef unsigned __int32 uint32;
+	typedef unsigned __int64 uint64;
+	typedef int ssize_t;
 #else
   #include <stdint.h>
   typedef uint8_t uint8;
--- Source/games/wow/wdc3file.cpp
+++ Source/games/wow/wdc3file.cpp
@@ -589,6 +589,54 @@ std::vector<std::string> WDC3File::get(unsigned int recordIndex, const core::Tab
         std::stringstream ss;
         ss << *reinterpret_cast<float *>(&val);
         result.push_back(ss.str());
+      } // new
+      else if (field->type == "int8")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<int8 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "uint8")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<uint8 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "int16")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<int16 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "uint16")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<uint16 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "int32")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<int32 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "uint32")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<uint32 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "int64")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<int64 *>(&val);
+          result.push_back(ss.str());
+      }
+      else if (field->type == "uint64")
+      {
+          std::stringstream ss;
+          ss << *reinterpret_cast<uint64 *>(&val);
+          result.push_back(ss.str());
       }
       else if (field->type == "int")
       {
@@ -596,22 +644,10 @@ std::vector<std::string> WDC3File::get(unsigned int recordIndex, const core::Tab
         ss << *reinterpret_cast<int32 *>(&val);
         result.push_back(ss.str());
       }
-      else if (field->type == "uint16")
-      {
-        std::stringstream ss;
-        ss << *reinterpret_cast<uint16 *>(&val);
-        result.push_back(ss.str());
-      }
       else if (field->type == "byte")
       {
         std::stringstream ss;
         ss << (*reinterpret_cast<uint16 *>(&val) & 0x000000FF);
-        result.push_back(ss.str());
-      }
-      else if (field->type == "uint64")
-      {
-        std::stringstream ss;
-        ss << *reinterpret_cast<long *>(&val);
         result.push_back(ss.str());
       }
       else
