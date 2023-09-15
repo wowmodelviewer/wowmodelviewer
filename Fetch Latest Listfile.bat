5dcc03982b82fdf0126def80592630cead268461
natnat <nathan_mathew_carr@hotmail.co.uk>
2023-09-15 03:35:18 +0100

    Updated Casclib

--- Fetch Latest Listfile.bat
+++ Fetch Latest Listfile.bat
@@ -3,8 +3,8 @@
 echo Deleting old listfile.csv if present.
 del listfile.csv
 
-echo Fetching latest listfile from https://github.com/wowdev/wow-listfile/raw/master/community-listfile.csv.
-curl -LO "https://github.com/wowdev/wow-listfile/raw/master/community-listfile.csv"
+echo Fetching latest listfile from https://github.com/wowdev/wow-listfile/releases/latest/download/community-listfile.csv.
+curl -LO "https://github.com/wowdev/wow-listfile/releases/latest/download/community-listfile.csv"
 
 echo renaming community-listfile.csv to listfile.csv
 ren community-listfile.csv listfile.csv
