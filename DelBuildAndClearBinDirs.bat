a1f0824c0428e6d419a85f2a8934500dc3b0dcf3
Nathan <nathan_mathew_carr@hotmail.co.uk>
2025-08-07 16:38:54 +0100

    Update DelBuildAndClearBinDirs.bat

--- DelBuildAndClearBinDirs.bat
+++ DelBuildAndClearBinDirs.bat
@@ -1,18 +1,24 @@
+@echo off
 rmdir /s /q build
 
 cd bin
 
-set "EXCEPTION_FILE=COPYING"
+set "EXCEPTION_FILE1=COPYING"
+set "EXCEPTION_FILE2=wowmodelviewer.exe - Shortcut.lnk"
 set "EXCEPTION_DIR=userSettings"
 
-for %%F in (*) do (
-    if /I not "%%F"=="%EXCEPTION_FILE%" (
+rem Loop through files only
+for /f "delims=" %%F in ('dir /b /a:-d') do (
+    if /I not "%%F"=="%EXCEPTION_FILE1%" if /I not "%%F"=="%EXCEPTION_FILE2%" (
+        echo Deleting file: %%F
         del "%%F"
     )
 )
 
-for /D %%D in (*) do (
+rem Loop through directories only
+for /d %%D in (*) do (
     if /I not "%%D"=="%EXCEPTION_DIR%" (
+        echo Deleting dir: %%D
         rd /s /q "%%D"
     )
 )
